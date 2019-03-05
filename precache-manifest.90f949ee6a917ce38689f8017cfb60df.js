2021bd506bf90cb665cddf43879743460e648308
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2019-03-05 09:18:33 +0700

    Updates

---
+++ precache-manifest.90f949ee6a917ce38689f8017cfb60df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/cryptocurrencies/static/media/logo.5c0436d5.png"
+  },
+  {
+    "revision": "fa91f5826982fbababa0",
+    "url": "/cryptocurrencies/static/js/runtime~main.fa91f582.js"
+  },
+  {
+    "revision": "7a685a1fb49ef3aa74e6",
+    "url": "/cryptocurrencies/static/js/main.7a685a1f.chunk.js"
+  },
+  {
+    "revision": "955c695d47dba326193b",
+    "url": "/cryptocurrencies/static/js/2.955c695d.chunk.js"
+  },
+  {
+    "revision": "7a685a1fb49ef3aa74e6",
+    "url": "/cryptocurrencies/static/css/main.d513432d.chunk.css"
+  },
+  {
+    "revision": "f52cb1885e9f8df04943955cbba608e5",
+    "url": "/cryptocurrencies/index.html"
+  }
+];
